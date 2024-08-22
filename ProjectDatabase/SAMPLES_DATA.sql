0b51a4ea5a01a6178424ff7ca555d0ea53dd5fda
nguyenconghieu12a2 <nguyenconghieu12a2@gmail.com>
2024-08-22 20:45:44 +0700

    task samples data

--- ProjectDatabase/SAMPLES_DATA.sql
+++ ProjectDatabase/SAMPLES_DATA.sql
@@ -1,8 +1,8 @@
 use ten_la_gi; --ten PROJECT
 go
 -- [user_site]
-insert into [user_site]([username], [gender], [birthday], [image], [email], [phone], [password], [signWithGoogle], [isDeleted]) values
-('qe', 'sf', '2004-11-10', 'vvdfvd', 'dfvfvd', '52346435', 'ssdfvsd', 1, 1);
+insert into [user_site]([username], [gender], [birthday], [image], [email], [phone], [password], [signWithGoogle], [isDeleted], [account_create_date]) values
+('qe', 'sf', '2004-11-10', 'vvdfvd', 'dfvfvd', '52346435', 'ssdfvsd', 1, 1, '2015-10-01');
 go
 select * from user_site
 -- [user_address]
@@ -13,14 +13,14 @@ go
 insert into [address]() values
 ();
 go
--- [country]
-insert into [country]() values
+-- [city]
+insert into [city]() values
 ();
 go
 -- 25 user, each user have 1 or 2 or 3 address
--- country just include ASIA country
+-- city in VietNam
 -- NOTE: fill random create_date
---============================================================================================================== 1ng
+--============================================================================================================== 1ng (Bao)
 -- [product_category]
 insert into [product_category]() values
 ();
@@ -34,7 +34,7 @@ insert into [sub_sub_Category]() values
 ();
 go
 -- 3 main category (CAKE, INGREDIENTS, materials), each main category seperate relevant sub category, with subcategory divide out more detail category
---============================================================================================================== 1ng duy nhat
+--============================================================================================================== 1ng duy nhat (Thanh)
 -- [product]
 insert into [product]() values
 ();
@@ -57,7 +57,7 @@ insert into [product_wishlist]() values
 go
 -- with each sub-sub-category --> create 10 products --> each product must have 1 big description, 4-7 detail description
 -- NOTE: product-wishlist: just fill 3-5 product for the first 5 accounts
---============================================================================================================== 1ng duy nhat
+--============================================================================================================== 1ng duy nhat (Thanh)
 -- [discount_category]
 insert into [discount_category]() values
 ();
@@ -71,12 +71,11 @@ insert into [coupons]() values
 ();
 go
 -- creative some discount event for some category or some discount for special days, creative some coupons to discount ship extra price or discount for product price
---==============================================================================================================
 -- [shipping_method]
 insert into [shipping_method]() values
 ();
 -- creative 5 shipping method Ex: giaohangtietkiem, giaohangsieutoc, etc.
---============================================================================================================== 1ng
+--============================================================================================================== 1ng (Bao)
 -- [order_status]
 insert into [order_status]() values
 ();
@@ -91,7 +90,7 @@ insert into [order_line]() values
 go
 -- 1 user has 2 shop order --> each order has 3-5 products
 -- about date fields in shop_order just fill only order_date (fill random date)
---============================================================================================================== 1ng duy nhat
+--============================================================================================================== 1ng duy nhat (Hieu)
 -- [payment_method]
 insert into [payment_method]() values
 ();
@@ -101,29 +100,30 @@ insert into [user_payment_method]() values
 ();
 go
 -- 2 main payment method: COD (cash on delivery), card --> NOTE: payment into must fit with order
---============================================================================================================== 1ng duy nhat
+--============================================================================================================== 1ng duy nhat (Hieu)
 -- [user_review_status]
-insert into [user_review_status]() values
+insert into [user_review_status]() values --pending, accepted, rejected
 ();
 go
 -- [user_review]
 insert into [user_review]() values
 ();
 go
--- based on order from user pick random product from random user to create some samples for user review table --> expected samples (50)
--- ideal review each product should have 2-3 review
---============================================================================================================== 1ng
+-- based on order from user pick random product from random user to create some samples for user review table --> expected samples (50-70), 5-7 review toxic , 10 pending
+-- ideal review each product should have 2-3 review, 1 vai sp ko co review
+--============================================================================================================== 1ng (Chinh)
 -- [banners]
 insert into [banners]() values
 ();
 go
+--5-7 banners
 -- [admin]
 insert into [admin]() values
 ();
 go
 -- banners have 5-7 image
 -- admin includes 4 of us
---============================================================================================================== 1ng
+--============================================================================================================== 1ng (Chinh)
 
 
 --NOTE: review from old samples
