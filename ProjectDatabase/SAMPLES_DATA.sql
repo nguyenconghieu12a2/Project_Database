df136b3a20ae11317ef32955a2d49ffbc8720d05
nguyenconghieu12a2 <nguyenconghieu12a2@gmail.com>
2024-08-28 13:04:33 +0700

    add usage to pro desTitle and format form

--- ProjectDatabase/SAMPLES_DATA.sql
+++ ProjectDatabase/SAMPLES_DATA.sql
@@ -1028,11 +1028,7 @@ insert into [admin]([fullname], [username], [email], [password], [avatar_img], [
 ('Nguyen Cong Hieu','nchieu', 'hieunc@gmail.com', 'ee608a70e3a536f1af3726bbd8bd0b55', 'male.jpg', 1), --nchieu
 ('Tran Gia Bao','tgbao', 'baotg@gmail.com', 'e405faf13f35eb5807579887f2e44220', 'male.jpg', 1), --tgbao
 ('Bach Cong Chinh','bcchinh' 'chinhbc@gmail.com', 'b985d6107f576d8fce6bfc9c3ac7d57a', 'male.jpg', 1), --bcchinh
-<<<<<<< HEAD
 ('Mai Phuc Thanh','mpthanh' 'thanhmp@gmail.com', 'e8dd6c0a56a63469dad05603a24531ee', 'male.jpg', 1); --mpthanh;
-=======
-('Mai Phuc Thanh','mpthanh' 'thanhmp@gmail.com', 'e8dd6c0a56a63469dad05603a24531ee', 'male.jpg', 1) --mpthanh;
->>>>>>> baone
 go
 -- banners have 5-7 image
 -- admin includes 4 of us
